df454fbabad8dbf474f6d8099d8e2965e7ecce26
Hugh Kelsey <hugh@45robots.com>
2015-08-05 12:10:35 -0700

    added token controller spec

--- spec/controllers/token_controller_spec.rb
+++ spec/controllers/token_controller_spec.rb
@@ -1,15 +1,16 @@
 require 'rails_helper'
 
 describe TokenController do
-  let(:open_tok) { double.as_null_object  }
+  let(:open_tok) { double(:open_tok, generate_token: 'token', create_session: double(:session, session_id: 'foo')) }
+
   before(:each) do
     stub_const("OPENTOK", open_tok)
   end
+
   describe "create" do
     it "should work" do
       post :create
       expect(response).to be_ok
     end
   end
-
 end
